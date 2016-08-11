9deff8de7660265600b0f40a4d7a4285bf4e6229
LightBulbWow <latefor12@naver.com>
2016-08-11 14:15:57 +0900

    yes

--- app/src/main/java/com/example/user/bdulgi/Bdulgi_List_edit.java
+++ app/src/main/java/com/example/user/bdulgi/Bdulgi_List_edit.java
@@ -1,14 +1,18 @@
 package com.example.user.bdulgi;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,11 +32,20 @@ import java.util.GregorianCalendar;
  */
 public class Bdulgi_List_edit extends AppCompatActivity implements onNetworkResponseListener {
 
+    public EditText using;
+    public EditText pricing;
+    public EditText dating;
+    public EditText briefing;
+    private Spinner spinner;
+    AccountTitleSpinnerList spinnerList;
     int year, month, day;
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listedit);
+
+
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
         toolbar.setTitle(" Expense Application");
@@ -44,7 +57,7 @@ public class Bdulgi_List_edit extends AppCompatActivity implements onNetworkResp
         GregorianCalendar calendar = new GregorianCalendar();
         year = calendar.get(Calendar.YEAR);
         month = calendar.get(Calendar.MONTH);
-        day= calendar.get(Calendar.DAY_OF_MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
 
         findViewById(R.id.When_save).setOnClickListener(new View.OnClickListener() {
 
@@ -56,13 +69,58 @@ public class Bdulgi_List_edit extends AppCompatActivity implements onNetworkResp
         getAccountList(null);
     }
 
+
+
+
+        public void toolbarRightButtonClick(View v) {
+            try{
+                using = (EditText) findViewById(R.id.use_text);
+                pricing = (EditText) findViewById(R.id.price_save);
+                dating = (EditText) findViewById(R.id.When_save);
+                briefing = (EditText) findViewById(R.id.brief_save);
+                spinner = (Spinner)findViewById(R.id.account_save);
+                if( TextUtils.isEmpty(using.getText())||
+                        TextUtils.isEmpty(pricing.getText())||
+                        TextUtils.isEmpty(dating.getText())||
+                        TextUtils.isEmpty(briefing.getText()))
+                {
+                    Toast.makeText(this , "Please 다시봐", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+
+                JSONObject requestObject = new JSONObject();
+                requestObject.put("PAYMENT_STORE_NM", using.getText().toString());
+                requestObject.put("PAYMENT_AMT", pricing.getText().toString());
+                requestObject.put("PAYMENT_DTTM", dating.getTag().toString());
+                requestObject.put("SUMMARY", briefing.getText().toString());
+                requestObject.put("ACCOUNT_TTL_CD", spinnerList.getAccountTitleCd(spinner.getSelectedItemPosition()));
+                requestObject.put("USER_ID", "test_user1");
+
+                Log.d("dilky", requestObject.toString());
+
+                commnetwork network = new commnetwork(this, this);
+                network.requestToServer("EXPENSE_I001",requestObject);
+
+
+            }
+
+             catch (Exception e) {
+                ErrorUtil.AlertException(this, "오류다", e);
+            }
+        }
+
+
+
     private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
 
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            String msg = String.format("%d / %d / %d", year,monthOfYear+1, dayOfMonth);
+            String msg = String.format("%d년%d월%d일", year,monthOfYear+1, dayOfMonth);
             TextView date = (TextView)findViewById(R.id.When_save);
             date.setText(msg);
+            date.setTag(String.format("%04d%02d%02d", year, monthOfYear + 1, dayOfMonth));
         }
     };
 
@@ -74,6 +132,7 @@ public class Bdulgi_List_edit extends AppCompatActivity implements onNetworkResp
 
 
             commnetwork commNetwork = new commnetwork(this, this);
+
             commNetwork.requestToServer("ACCOUNT_L001", req_data);
         } catch (Exception e) {
             ErrorUtil.AlertException(this, "오류다", e);
@@ -95,26 +154,45 @@ public class Bdulgi_List_edit extends AppCompatActivity implements onNetworkResp
     }
     @Override
     public void onSuccess(String api_key, JSONObject response) throws JSONException {
-        Toast.makeText(this, "요청 성공",Toast.LENGTH_SHORT).show();
-        JSONArray array = response.getJSONArray("REC");
+        try{
+        if("ACCOUNT_L001".equals(api_key)) {
 
-        try {
-            AccountTitleSpinnerList spinnerList = new AccountTitleSpinnerList(array);
+            Toast.makeText(this, "요청 성공", Toast.LENGTH_SHORT).show();
+
+            JSONArray array = response.getJSONArray("REC");
+            spinnerList = new AccountTitleSpinnerList(array);
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(Bdulgi_List_edit.this, android.R.layout.simple_spinner_item, spinnerList.getArrayList());
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            Spinner spinner = (Spinner) findViewById(R.id.account_save);
+            spinner.setAdapter(adapter);
+        }else if("EXPENSE_I001".equals(api_key))
+            {
+                Toast.makeText(this, "보내기 성공", Toast.LENGTH_SHORT).show();
+                if(!TextUtils.isEmpty(response.getString("EXPENSE_SEQ"))){
+                    Intent intent = new Intent(Bdulgi_List_edit.this, Bdulgi_List.class);
+                    intent.putExtra("EXPENSE_SEQ", response.getString("EXPENSE_SEQ"));
+                    startActivity(intent);
+                    finish();
+                }
+            }
 
 
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(Bdulgi_List_edit.this, android.R.layout.simple_spinner_item, spinnerList.getArrayList());
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner spinner = (Spinner) findViewById(R.id.account_save);
-        spinner.setAdapter(adapter);
-        } catch (Exception e) {
-            e.printStackTrace();
+        }
+        catch (Exception e) {
+            ErrorUtil.AlertException(this, "오류입니다", e);
+
         }
     }
 
     @Override
     public void onFailure(String api_key, String error_cd, String error_msg) {
-        Toast.makeText(this, "요청 실패",Toast.LENGTH_SHORT).show();
+        if("ACCOUNT_L001".equals(api_key))
+            Toast.makeText(this, "요청 실패",Toast.LENGTH_SHORT).show();
+        else if("EXPENSE_I001".equals(api_key))
+        {
+
+        }
     }
 
 }
